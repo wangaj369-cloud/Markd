3ed6994370c73bca49bc94f4557ac2a8c5876c5f
Kagawa <wangaj369@gmail.com>
2026-07-23 00:02:38 +0100

    commit app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,6 @@ const [examQuestionCount, setExamQuestionCount] = useState(5);
 const [examQuestions, setExamQuestions] = useState([]);
 const [examAnswers, setExamAnswers] = useState({});
 const [completedExam,setCompletedExam] = useState(null);
-const [examMark,setExamMark] = useState(null);
 const [customQuestionCount,setCustomQuestionCount] = useState("");
 const [examDifficulty, setExamDifficulty] = useState("Mixed");
 const examSettings = {
