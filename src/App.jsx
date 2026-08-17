482dd5d0102bbc83692723439d57f8f3e85ab1aa
Kagawa <wangaj369@gmail.com>
2026-08-17 14:13:13 +0100

    commit app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { supabase } from "./supabase";
 import LoginPage from "./components/LoginPage";
-import LandingPage from "./components/Landingpage.jsx";
+import LandingPage from "./components/Landingpage";
 import { useState, useEffect } from "react";
 import SetupPage from "./components/setuppage";
 import DashboardPage from "./components/dashboardpage";
